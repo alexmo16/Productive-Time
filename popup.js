f2fed4b2da1721cbdb11f15f1a6de145de6239a4
alexmo16 <alexis.morel909@gmail.com>
2018-05-31 21:20:13 -0400

    input value and start button state stored in chrome storage

--- popup.js
+++ popup.js
@@ -1,10 +1,26 @@
-let changeColor = document.getElementById('changeColor');
-
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+window.onload = function() {
+    var startButton = document.getElementById('timerButton');
+    var timeInput = document.getElementById('time');
+    chrome.storage.sync.get('time', function(response) {
+        console.log(response.time.value);
+        var timeValue = 0;
+        if (response.time && ( response.time.value != "" || response.time.value != undefined )) {
+            timeValue =  response.time.value;
+        }
+        timeInput.value = timeValue;
+        
+        chrome.storage.sync.get('isDisable', function(response) {
+            var isDisable = response.isDisable ? response.isDisable.value : false;
+            startButton.disabled = isDisable;
+        });
     });
-};
+//
+    startButton.onclick = function(element) {
+        var timeValue = timeInput.value;
+        var timeObject = {'value': timeValue};
+        var buttonStateObject = {'value': true};
+        chrome.storage.sync.set({'time': timeObject, 'isDisable': buttonStateObject}, function() {
+            startButton.disabled = true;
+        });
+    };
+}
