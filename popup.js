7412d6bd5acc80852d73cecf22365e120dfb162d
alexmo16 <alexis.morel909@gmail.com>
2018-06-14 20:35:18 -0400

    disabled button and get all variables

--- popup.js
+++ popup.js
@@ -1,20 +1,20 @@
 window.onload = function() {
     var startButton = document.getElementById('timerButton');
     var timeInput = document.getElementById('time');
-    chrome.storage.sync.get('time', function(response) {
+    chrome.storage.sync.get(null, function(response) {
         console.log(response.time.value);
+        console.log(response.isDisable.value);
+
         var timeValue = 0;
         if (response.time && ( response.time.value != "" || response.time.value != undefined )) {
             timeValue =  response.time.value;
         }
         timeInput.value = timeValue;
         
-        chrome.storage.sync.get('isDisable', function(response) {
-            var isDisable = response.isDisable ? response.isDisable.value : false;
-            startButton.disabled = isDisable;
-        });
+        var isDisable = response.isDisable ? response.isDisable.value : false;
+        startButton.disabled = isDisable;
     });
-//
+
     startButton.onclick = function(element) {
         var timeValue = timeInput.value;
         var timeObject = {'value': timeValue};
