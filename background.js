6bb0331da3207e95b9670552b5d3044e2c3ccf39
alexmo16 <alexis.morel909@gmail.com>
2018-06-14 22:10:52 -0400

    add timer and reset popup when done

--- background.js
+++ background.js
@@ -1,4 +1,20 @@
-chrome.runtime.onInstalled.addListener(function() {
+  // listener who check if the state of startTimer changed in the storage.    
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (key in changes) {
+        if (key == 'startTimer') {
+            var startTimer = changes[key].newValue;
+            if (startTimer) {
+                chrome.storage.sync.set({'startTimer': {'value': false}}, function() {
+                    var currentTime = new Date();
+                    chrome.storage.sync.get(['time'], function(result) {
+                        chrome.alarms.create('productiveTimer', {delayInMinutes: result.time.value});
+                    });                    
+                });
+            }
+        } 
+    }
+});
 
-
+chrome.alarms.onAlarm.addListener(function( alarm ) {
+    chrome.storage.sync.set({'time': {'value': 0}, 'isDisable': {'value': false}}, function() {});
 });
--- popup.js
+++ popup.js
@@ -2,8 +2,11 @@ window.onload = function() {
     var startButton = document.getElementById('timerButton');
     var timeInput = document.getElementById('time');
     chrome.storage.sync.get(null, function(response) {
-        console.log(response.time.value);
-        console.log(response.isDisable.value);
+        
+        if (response.time && response.isDisable) {
+            console.log(response.time.value);
+            console.log(response.isDisable.value);
+        }
 
         var timeValue = 0;
         if (response.time && ( response.time.value != "" || response.time.value != undefined )) {
@@ -16,10 +19,11 @@ window.onload = function() {
     });
 
     startButton.onclick = function(element) {
-        var timeValue = timeInput.value;
+        var timeValue = parseInt(timeInput.value);
         var timeObject = {'value': timeValue};
         var buttonStateObject = {'value': true};
-        chrome.storage.sync.set({'time': timeObject, 'isDisable': buttonStateObject}, function() {
+        var startTimerObject = {'value': true};
+        chrome.storage.sync.set({'time': timeObject, 'isDisable': buttonStateObject, 'startTimer': startTimerObject}, function() {
             startButton.disabled = true;
         });
     };
